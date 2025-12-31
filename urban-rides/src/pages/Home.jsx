a3371ead00adc7f988740ab8c4267f5aa4a565ca
Darshan Karagir <karagirdarshan1@gmail.com>
2025-12-31 18:50:54 +0530

    new pages add

--- urban-rides/src/pages/Home.jsx
+++ urban-rides/src/pages/Home.jsx
@@ -22,9 +22,69 @@ const Home = () => {
           </div>
         </div>
 
-        <div className={styles.Home}>
+        {/* Gap section */}
+        <section className={styles.gapSection}>
+          <h2 className={styles.gapTitle}>
+            One Platform. Two <span className={styles.gapTextItalic}>Possibilities</span>.
+          </h2>
 
-        </div>
+          <p className={styles.gapText}>
+            Rent your desired car or list your own car and start earning effortlessly.
+          </p>
+        </section>
+
+        {/* Options section */}
+        <section className={styles.optionsSection}>
+          <div className={styles.optionBox}>
+            <div className={styles.renter}>
+              <h3 className={styles.optionTitle}>Renter</h3>
+              <div className={styles.optionImgRenter}></div>
+              <p className={styles.discripion}>Book verified cars instantly with transparent pricing and zero hassle</p>
+            </div>
+
+            <div className={styles.owner}>
+              <h3 className={styles.optionTitle}>Owner</h3>
+              <div className={styles.optionImgOwner}></div>
+              <p className={styles.discripion}>List your own car and start earning effortlessly. Get fair pricing for your vehicle.</p>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.WhyUsSection}>
+          <h2 className={styles.WhyUsTitle}>Why Choose Urban Rides?</h2>
+          <p className={styles.WhyUsText}>
+            At Urban Rides, we prioritize your convenience and satisfaction. Our platform offers a seamless experience for both renters and owners, ensuring that you have access to reliable vehicles and fair pricing. Join us today and discover the difference!
+          </p>
+
+          {/* Auto‑scrolling row */}
+          <div className={styles.scrollContainer}>
+            <div className={styles.scrollContent}>
+              <div className={styles.item}>
+                <p>TRUSTED SUPPORT</p>
+              </div>
+              <div className={styles.item}>
+
+                <p>INSTANT BOOKINGS</p>
+              </div>
+              <div className={styles.item}>
+                <p>100% VERIFIED CARS</p>
+              </div>
+
+              {/* duplicate set for seamless loop */}
+              <div className={styles.item}>
+                <p>EARN EFFORTLESSLY</p>
+              </div>
+              <div className={styles.item}>
+                <p>SEAMLESS EXPRIENCE</p>
+              </div>
+              <div className={styles.item}>
+                <p>PREMIUM, PRICED RIGHT</p>
+              </div>
+            </div>
+          </div>
+
+        </section>
+        
 
       </main>
 
