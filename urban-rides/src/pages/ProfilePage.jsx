adcc55e75273fec55b5adb7eb2ea6d3e282ea2e1
Darshan Karagir <karagirdarshan1@gmail.com>
2026-02-22 21:35:02 +0530

    fix

--- urban-rides/src/pages/ProfilePage.jsx
+++ urban-rides/src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from "./profilePage.module.css.module.css"
+import styles from "./profile.module.css"
 import ProfileCard from '../components/ProfileCard'
 
 const ProfilePage = () => {
